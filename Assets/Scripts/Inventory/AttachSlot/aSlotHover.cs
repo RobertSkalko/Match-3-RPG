14c4f3d8ef5f03d01574400779173beb5dc2d7ad
robert trebor <treborx555@gmail.com>
2018-06-24 15:42:01 +0200

    bit better tooltip

--- Assets/Scripts/Inventory/AttachSlot/aSlotHover.cs
+++ Assets/Scripts/Inventory/AttachSlot/aSlotHover.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class aSlotHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -25,75 +22,13 @@ public class aSlotHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
             Destroy(SlotData.tooltip);
         }
 
-        SlotData.tooltip = Instantiate(Prefabs.ItemTooltip);
-
-        SlotData.tooltip.transform.SetParent(aSlotData.canvas.transform, false);
-
-        setFields(SlotData.tooltip);
-
-        setXPositionOfTooltip(data);
-
-        setYPositionOfTooltip(data);
+        summonTooltip();
     }
 
-    private void setFields(GameObject tooltip)
+    private void summonTooltip()
     {
-        GameObject viewport = tooltip.transform.Find("Viewport").gameObject;
-        GameObject contents = viewport.transform.Find("Contents").gameObject;
-        GameObject name = contents.transform.Find("Name").gameObject;
-        GameObject stats = contents.transform.Find("Stats").gameObject;
-        GameObject desc = contents.transform.Find("Desc").gameObject;
-
-        name.GetComponent<Text>().text = SlotData.Slot.item.name;
-    }
-
-    private void setXPositionOfTooltip(PointerEventData data)
-    {
-        Vector2 mousePos = data.position;
-
-        int slotWidth = (int)(gameObject.GetComponent<RectTransform>().rect.width / 2 * gameObject.GetComponent<RectTransform>().lossyScale.x);
-        int tooltipWidth = (int)(SlotData.tooltip.GetComponent<RectTransform>().rect.width / 2 * SlotData.tooltip.GetComponent<RectTransform>().lossyScale.x);
-        int offsetWidth = slotWidth + tooltipWidth;
-
-        bool isLeftSideOfScreen = true;
-
-        if (mousePos.x > Screen.width / 2) { isLeftSideOfScreen = false; }
-
-        int pushElementLeftOrRight = 0;
-
-        if (isLeftSideOfScreen)
-        {
-            pushElementLeftOrRight = offsetWidth;
-        }
-        else
-        {
-            pushElementLeftOrRight = -offsetWidth;
-        }
-
-        Vector2 tooltipPos = gameObject.transform.position;
-        tooltipPos.x = tooltipPos.x + pushElementLeftOrRight;
-
-        SlotData.tooltip.transform.position = tooltipPos;
-    }
-
-    private void setYPositionOfTooltip(PointerEventData data)
-    {
-        int tooltipHeight = (int)(SlotData.tooltip.GetComponent<RectTransform>().rect.height * SlotData.tooltip.GetComponent<RectTransform>().lossyScale.y);
-
-        if (data.position.y > Screen.height / 2)
-        {
-            if (SlotData.CurrentObj.transform.position.y + tooltipHeight / 2 > Screen.height)
-            {
-                SlotData.tooltip.transform.position = new Vector2(SlotData.tooltip.transform.position.x, Screen.height - tooltipHeight / 2);
-            }
-        }
-        else
-        {
-            if (SlotData.CurrentObj.transform.position.y - tooltipHeight / 2 < 0)
-            {
-                SlotData.tooltip.transform.position = new Vector2(SlotData.tooltip.transform.position.x, 0 + tooltipHeight / 2);
-            }
-        }
+        SlotData.tooltip = Instantiate(Prefabs.ItemTooltip, GameObject.Find("Canvas").transform, false);
+        SlotData.tooltip.GetComponent<aTooltip>().SlotData = SlotData;
     }
 
     public void OnPointerExit(PointerEventData data)
---
+++ Assets/Scripts/Inventory/AttachTooltip/aTooltip.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class aTooltip : MonoBehaviour
+{
+    public aSlotData SlotData;
+
+    private GameObject viewportObj;
+    private GameObject contentsObj;
+    private GameObject nameObj;
+    private GameObject statsObj;
+    private GameObject descObj;
+
+    private void Start()
+    {
+        setFields();
+
+        setXPositionOfTooltip();
+
+        setYPositionOfTooltip();
+    }
+
+    private void setFields()
+    {
+        viewportObj = this.gameObject.transform.Find("Viewport").gameObject;
+        contentsObj = viewportObj.transform.Find("Contents").gameObject;
+        nameObj = contentsObj.transform.Find("Name").gameObject;
+        statsObj = contentsObj.transform.Find("Stats").gameObject;
+        descObj = contentsObj.transform.Find("Desc").gameObject;
+
+        nameObj.GetComponent<Text>().text = SlotData.Slot.item.name;
+    }
+
+    private void setXPositionOfTooltip()
+    {
+        int slotWidth = (int)(SlotData.CurrentObj.GetComponent<RectTransform>().rect.width / 2 * SlotData.CurrentObj.GetComponent<RectTransform>().lossyScale.x);
+        int tooltipWidth = (int)(SlotData.tooltip.GetComponent<RectTransform>().rect.width / 2 * SlotData.tooltip.GetComponent<RectTransform>().lossyScale.x);
+        int offsetWidth = slotWidth + tooltipWidth;
+
+        bool isLeftSideOfScreen = true;
+
+        if (Input.mousePosition.x > Screen.width / 2) { isLeftSideOfScreen = false; }
+
+        int pushElementLeftOrRight = 0;
+
+        if (isLeftSideOfScreen)
+        {
+            pushElementLeftOrRight = offsetWidth;
+        }
+        else
+        {
+            pushElementLeftOrRight = -offsetWidth;
+        }
+
+        Vector2 tooltipPos = SlotData.CurrentObj.transform.position;
+        tooltipPos.x = tooltipPos.x + pushElementLeftOrRight;
+
+        SlotData.tooltip.transform.position = tooltipPos;
+    }
+
+    private void setYPositionOfTooltip()
+    {
+        int tooltipHeight = (int)(SlotData.tooltip.GetComponent<RectTransform>().rect.height * SlotData.tooltip.GetComponent<RectTransform>().lossyScale.y);
+
+        if (Input.mousePosition.y > Screen.height / 2)
+        {
+            if (SlotData.CurrentObj.transform.position.y + tooltipHeight / 2 > Screen.height)
+            {
+                SlotData.tooltip.transform.position = new Vector2(SlotData.tooltip.transform.position.x, Screen.height - Screen.height / 50 - tooltipHeight / 2);
+            }
+        }
+        else
+        {
+            if (SlotData.CurrentObj.transform.position.y - tooltipHeight / 2 < 0)
+            {
+                SlotData.tooltip.transform.position = new Vector2(SlotData.tooltip.transform.position.x, Screen.height / 50 + tooltipHeight / 2);
+            }
+        }
+    }
+}
